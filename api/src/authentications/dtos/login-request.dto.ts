b548d0c56112c8c24e2d2f0ff170739caba45725
dchantrel <dchantrel@portalp.com>
2026-04-13 11:59:37 +0000

    Av

--- api/src/authentications/dtos/login-request.dto.ts
+++ api/src/authentications/dtos/login-request.dto.ts
@@ -1,21 +1,26 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsBoolean, IsEmail, IsString, MinLength } from 'class-validator';
 
 export class LoginRequestDto {
   @ApiProperty({
     description: `User's email`,
     required: true,
   })
+  @IsEmail()
   email: string;
 
   @ApiProperty({
     description: `User's password`,
     required: true,
   })
+  @IsString()
+  @MinLength(1)
   password: string;
 
   @ApiProperty({
     description: `Remember me option`,
     required: true,
   })
+  @IsBoolean()
   rememberMe: boolean;
 }
